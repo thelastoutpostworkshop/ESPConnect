61f8139ed656cc60a3b078142b57d786c488005c
thelastoutpostworkshop <thelastoutpostworkshop@gmail.com>
2025-12-07 16:08:31 -0500

    connectAndHandshake

--- src/services/esptoolClient.ts
+++ src/services/esptoolClient.ts
@@ -429,17 +429,8 @@ export function createEsptoolClient({
         transport.baudrate = desiredBaud;
       }
 
-      // const chipFamily =
-      //   typeof (loader as any).getChipFamily === 'function'
-      //     ? (loader as any).getChipFamily()
-      //     : (loader as any).chipFamily;
-      // const chip = chipFamily
-      //   ? {
-      //     CHIP_NAME: chipName,
-      //     SPI_REG_BASE: getSpiFlashAddresses(chipFamily).regBase,
-      //   }
-      //   : { CHIP_NAME: chipName };
-      // const chipFamily = loader.getChipFamily();
+      // const securityInfo = await loader.getSecurityInfo();
+
 
       status('Reading chip information...');
       return { chipName, macAddress };
