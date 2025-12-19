9f0c52243b6848065272c0061b6ada70f1e17876
Meat_Suger <3544883297@qq.com>
2025-12-19 21:16:26 +0800

    add vue-i18n dependencie

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import { createI18n } from 'vue-i18n';
+import { messages, detectLocale } from './locales';
 
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
@@ -14,4 +16,11 @@ const vuetify = createVuetify({
   directives,
 });
 
-createApp(App).use(vuetify).mount('#app');
+const i18n = createI18n({
+  legacy: false,
+  locale: (typeof window !== 'undefined' && window.localStorage.getItem('esp32-locale')) || detectLocale(),
+  fallbackLocale: 'en',
+  messages,
+});
+
+createApp(App).use(vuetify).use(i18n).mount('#app');
